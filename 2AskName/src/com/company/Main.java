f2f978e39be50fe20e0c1e741d8dc1fe84472007
udip <udipdhungel123@gmail.com>
2019-11-26 11:43:32 -0600

    1stCommit

---
+++ 2AskName/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("Enter your name: ");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input);
+
+    }
+}
